283bcc2557f23000f64efb090e60f206cc7148d5
Melanie Ryan <melaniehoperyan@gmail.com>
2018-12-12 22:33:34 -0800

    almost there with challenge 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,31 +28,31 @@ function displayInventory() {
     });
 }
 
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
 function takeOrder() {
     inquirer.prompt([
-        {
+        {// The first should ask them the ID of the product they would like to buy.
             name: "item",
             message: "What is the item ID of your desired purchase?",
             type: "input",
-        }, {
+        }, {// The second message should ask how many units of the product they would like to buy.
             name: "quantity",
             message: "How many would you like to purchase?",
         }
     ]).then((res) => {
         console.log(res);
-        if (user.quantity < item.quantity) {
-            console.log("Your order has been fulfilled!");
-        } else if (user.quantity > item.quantiy) {
-            console.log("Sorry! We only have " + item.quantity + "left.");
-        }
+        // Retrieve quantity from products in sql & compare to customer request
+        // If in stock, fulfill customer order / update SQL database to reflect the remaining quantity
+        connection.query("SELECT quantity FROM products WHERE product_name = ?", [res.item], (res, data) => {
+            if (data.quantity > res.quantity) {
+                console.log("Your order has been fulfilled!");
+            } // If insufficient quantity!, prevent order
+            else if (data.quantity < res.quantity) {
+                console.log("Sorry! We only have " + data.quantity + "left.");
+            }
+            // Once the update goes through, show the customer the total cost of their purchase
+            // connection.end();
+        })
     })
 };
 
 
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase
